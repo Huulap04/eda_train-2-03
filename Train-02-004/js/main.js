fff0951d44cf097229af38b255c5b5ff3729b22d
Vo Thanh Vuong <vuongvt.ce190182@gmail.com>
2026-06-15 02:32:59 +0700

    hoàn thành tất cả asset, chưa có hiệu ứng

--- Train-02-004/js/main.js
+++ Train-02-004/js/main.js
@@ -8,10 +8,10 @@ $(document).ready(function () {
     var el = document.getElementById("container");
     swipedetect(el, function (swipedir) {
       if (swipedir == "left") {
-        window.goToSlide("002");
+        window.goToSlide("005");
       }
-      if (swipedir == "right") {    
-        disableSwipe = true;
+      if (swipedir == "right") {
+        window.goToSlide("003");
       }
     });
   
