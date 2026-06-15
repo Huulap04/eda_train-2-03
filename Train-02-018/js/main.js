ba068d568c9d11e347a5aee6e9a5856a12617f8b
chuongnhwork-cmd <chuongnh.work@gmail.com>
2026-06-15 14:37:49 +0700

    done

---
+++ Train-02-018/js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    // setting slide
+    currentSlide = "001";
+    localStorage.setItem("current-slide", currentSlide);
+    disableSwipe = false;
+  
+    // flow-slide
+    var el = document.getElementById("container");
+    swipedetect(el, function (swipedir) {
+      if (swipedir == "left") {
+        window.goToSlide("019");
+      }
+      if (swipedir == "right") {    
+         window.goToSlide("017");
+      }
+    });
+  
+    // animation here
+    setTimeout(() => {
+      $(".line-chart-container").addClass("animate"); //this line only for example
+    }, 500);
+  });
+  
