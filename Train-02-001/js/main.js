445d3ef62d358ea57629aec3733cc2535db80a41
Vo Thanh Vuong <vuongvt.ce190182@gmail.com>
2026-06-15 14:09:39 +0700

    cập nhật hiệu ứng cho 001

--- Train-02-001/js/main.js
+++ Train-02-001/js/main.js
@@ -1,23 +1,27 @@
 $(document).ready(function () {
-    // setting slide
-    currentSlide = "001";
-    localStorage.setItem("current-slide", currentSlide);
-    disableSwipe = false;
-  
-    // flow-slide
-    var el = document.getElementById("container");
-    swipedetect(el, function (swipedir) {
-      if (swipedir == "left") {
-        window.goToSlide("002");
-      }
-      // if (swipedir == "right") {    
-      //   disableSwipe = true;
-      // }
-    });
-  
-    // animation here
-    setTimeout(() => {
-      $(".line").addClass("show"); //this line only for example
-    }, 500);
+  // setting slide
+  currentSlide = "001";
+  localStorage.setItem("current-slide", currentSlide);
+  disableSwipe = false;
+
+  // flow-slide
+  var el = document.getElementById("container");
+  swipedetect(el, function (swipedir) {
+    if (swipedir == "left") {
+      window.goToSlide("002");
+    }
+    // if (swipedir == "right") {    
+    //   disableSwipe = true;
+    // }
   });
-  
+
+  // animation here
+  const animations = {
+    300: [".intro-text"],
+    500: [".hero-subtitle-box", ".hero-content-box", ".text-title-highlight"],
+  };
+
+  for (const [offset, elem] of Object.entries(animations)) {
+    setTimeout(() => elem.forEach((e) => $(e).addClass("show")), offset);
+  }
+});
