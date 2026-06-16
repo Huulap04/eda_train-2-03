d9ca8ca96cb858486ef3bed24703ffca0e949509
Vo Thanh Vuong <vuongvt.ce190182@gmail.com>
2026-06-16 13:24:27 +0700

    hoàn thành toàn bộ hiệu ứng

--- Train-02-004/js/main.js
+++ Train-02-004/js/main.js
@@ -16,8 +16,36 @@ $(document).ready(function () {
     });
   
     // animation here
-    setTimeout(() => {
-      $(".line").addClass("show"); //this line only for example
-    }, 500);
+var blocks = [
+    { icon: ".content-icon",   title: ".title",   sub: ".content-sub-title"   },
+    { icon: ".content-icon-1", title: ".title-1", sub: ".content-sub-title-1" },
+    { icon: ".content-icon-2", title: ".title-2", sub: ".content-sub-title-2" },
+    { icon: ".content-icon-3", title: ".title-3", sub: ".content-sub-title-3" },
+];
+
+blocks.forEach(function (block, blockIndex) {
+    var baseDelay = 300 + blockIndex * 160;
+
+    // Icon scale-in
+    setTimeout(function () {
+        $(block.icon).addClass("show");
+    }, baseDelay);
+
+    // Title stroke flash
+    setTimeout(function () {
+        $(block.title).addClass("show");
+    }, baseDelay + 100);
+
+    // Sub items stagger
+    var items = $(block.sub).find("span").add(
+        blockIndex === 1 ? $(".disadvantage-title, .disadvantage-content span") : $()
+    );
+    items.each(function (i) {
+        var el = this;
+        setTimeout(function () {
+            $(el).addClass("show");
+        }, baseDelay + 220 + i * 80);
+    });
+});
   });
   
--- Train-02-006/js/main.js
+++ Train-02-006/js/main.js
@@ -1,23 +1,45 @@
 $(document).ready(function () {
-    // setting slide
-    currentSlide = "001";
-    localStorage.setItem("current-slide", currentSlide);
-    disableSwipe = false;
-  
-    // flow-slide
-    var el = document.getElementById("container");
-    swipedetect(el, function (swipedir) {
-      if (swipedir == "left") {
-        window.goToSlide("007");
-      }
-      if (swipedir == "right") {    
-        window.goToSlide("005")
-      }
-    });
-  
-    // animation here
-    setTimeout(() => {
-      $(".line").addClass("show"); //this line only for example
-    }, 500);
+  // setting slide
+  currentSlide = "001";
+  localStorage.setItem("current-slide", currentSlide);
+  disableSwipe = false;
+
+  // flow-slide
+  var el = document.getElementById("container");
+  swipedetect(el, function (swipedir) {
+    if (swipedir == "left") {
+      window.goToSlide("007");
+    }
+    if (swipedir == "right") {
+      window.goToSlide("005")
+    }
   });
-  
+
+  // animation here
+  setTimeout(function () {
+    $(".preserve-left-bg").addClass("show");
+  }, 200);
+
+  setTimeout(function () {
+    $(".preserve-right").addClass("show");
+  }, 250);
+
+  setTimeout(function () {
+    $(".preserve-heading").addClass("show");
+  }, 400);
+
+  setTimeout(function () {
+    $(".preserve-card-title").addClass("show");
+  }, 550);
+
+  $(".preserve-item").each(function (i) {
+    var el = this;
+    setTimeout(function () {
+      $(el).addClass("show");
+    }, 700 + i * 120);
+  });
+
+  setTimeout(function () {
+    $(".preserve-product").addClass("show");
+  }, 600);
+});
